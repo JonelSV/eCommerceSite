8c30694e97f8bf59c3ea58b73ca927bfbe37acb3
JonelSV <svjonel@gmail.com>
2021-09-10 13:49:23 -0400

    first commit

---
+++ src/components/Homepage.js
@@ -0,0 +1,37 @@
+import React from 'react' 
+
+
+const Homepage = () => {
+    return (
+  <div className='homepage'>
+      <div className="directory">
+          <div className="item">
+              <div className="content">
+                  <h1 className="title">Hats</h1>
+                  <h3 className="subtitle">Shop Now</h3>
+              </div>
+          </div>
+          <div className="item">
+              <div className="content">
+                  <h1 className="title">Jackets</h1>
+                  <h3 className="subtitle">Shop Now</h3>
+              </div>
+          </div>
+          <div className="item">
+              <div className="content">
+                  <h1 className="title">Sneakers</h1>
+                  <h3 className="subtitle">Shop Now</h3>
+              </div>
+          </div>
+          <div className="item">
+              <div className="content">
+                  <h1 className="title">Shirts</h1>
+                  <h3 className="subtitle">Shop Now</h3>
+              </div>
+          </div>
+      </div>
+  </div>
+    )}
+
+
+export default Homepage;

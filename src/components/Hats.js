3d0282f10e664079f14b06d4830a0f9ae0f6af9c
JonelSV <svjonel@gmail.com>
2021-09-12 16:26:15 -0400

    'commit'

--- src/components/Hats.js
+++ src/components/Hats.js
@@ -1,11 +1,13 @@
 import React from 'react' 
+import { Link } from 'react-router-dom';
 
 
 const Hats = () => {
     return (
         <div>
+          <Link to='hats'>
             <h2>Hats</h2>
-
+          </Link>
         </div>
     )
 }

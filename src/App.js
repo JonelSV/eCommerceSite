fba88c0ddffc86144619e411c9a022ecee00d929
JonelSV <svjonel@gmail.com>
2021-09-12 16:01:12 -0400

    'commit'

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from 'react'
 import { Switch, Route } from 'react-router';
 import Homepage from './pages/Homepage';
 import './App.css'
+import Hats from './components/Hats';
 
 
 const App = () => {
+  const HatsPage = () => {
+    <div>
+      <h2>Hats</h2>
+    </div>
+  }
+
+
   return (
     <div>
-      
-      <Homepage />
+      <Switch>
+        <Route exact path='/hats' component={Hats} />
+        <Route exact path= '/' component= {Homepage}/>
+      </Switch>
       
     </div>
 
--- src/components/Items.js
+++ src/components/Items.js
@@ -5,14 +5,14 @@ import { withRouter } from 'react-router';
 const Items = ({title, imageUrl, history, linkUrl, match}) => {
     return (
 
-        <div onClick= {() => history.push(`${match.url}${linkUrl}`)}>
+       
             <div className="item" style= {{backgroundImage: `url(${imageUrl})`}}>
               <div className="content">
-                  <h1 className="title">{title}</h1>
+                  <h1 className="title">{title.toUpperCase()}</h1>
                   <h3 className="subtitle">Shop Now</h3>
               </div>
             </div>
-        </div>
+        
     )
 }
 

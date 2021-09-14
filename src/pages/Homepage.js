471baa3d5986b409f3091a902b41b0f05e8fd202
JonelSV <svjonel@gmail.com>
2021-09-13 20:01:01 -0400

    'created navbar'

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,12 @@
 import React from 'react' 
 import Directory from '../components/Directory';
+import Navbar from './Navbar';
 
 
 const Homepage = () => {
     return (
   <div className='homepage'>
+      <Navbar />
       <Directory />
   </div>
     

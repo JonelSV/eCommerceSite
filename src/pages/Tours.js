7d69dc0167133a4da8255660b76f05ce085e0803
JonelSV <svjonel@gmail.com>
2021-09-14 07:28:36 -0400

    'tours components'

---
+++ src/pages/Tours.js
@@ -0,0 +1,16 @@
+import React from 'react' 
+import TourPackages from '../components/TourPackages'
+
+
+const Tours = () => {
+    return(
+
+        <div className="tours">
+
+            <TourPackages/> 
+
+        </div>
+    )
+}
+
+export default Tours;

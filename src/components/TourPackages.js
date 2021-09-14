3213802006f7d11b9a3c7c6fef18ebbb95f26944
JonelSV <svjonel@gmail.com>
2021-09-14 07:51:39 -0400

    'tourpackage css'

--- src/components/TourPackages.js
+++ src/components/TourPackages.js
@@ -1,25 +1,40 @@
 import React from 'react' 
+import 'bootstrap/dist/css/bootstrap.min.css'
 import {Card, Button} from 'react-bootstrap'
 
 const TourPackages = () => {
     return (
-        <div>
-            <Card style={{color: "#008080" }}>
-               <Card.Img src="https://picsum.photos/200/300"/>
-                <Card.Body>
-                    <Card.Title>
-                        Tour Package 1
-                    </Card.Title>.
-                    <Card.Text>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, vero!
-                    </Card.Text>
-                </Card.Body>
-                <Button variant='secondary'> Click for more details</Button>
-            </Card>
-
-
-
-
+        <div className ='row'>
+            <div className="col-md-6">
+                <Card style={{color: "#008080" }}>
+                <Card.Img src="https://picsum.photos/1920/1080?random=1"/>
+                    <Card.Body>
+                        <Card.Title>
+                            Tour Package 1
+                        </Card.Title>.
+                        <Card.Text>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, vero!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, vero!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, vero!
+                        </Card.Text>
+                    </Card.Body>
+                    <Button variant='secondary'> Click for more details</Button>
+                </Card>
+                <Card style={{color: "#008080" }}>
+                <Card.Img src="https://picsum.photos/1920/1080?random=2"/>
+                    <Card.Body>
+                        <Card.Title>
+                            Tour Package 2
+                        </Card.Title>.
+                        <Card.Text>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, vero!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, vero!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, vero!
+                        </Card.Text>
+                    </Card.Body>
+                    <Button variant='secondary'> Click for more details</Button>
+                </Card>
+            </div>
         </div>
     )
 }

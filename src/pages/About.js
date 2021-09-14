dce36a4353e86c7ef35e427678a93ac93d4f7712
JonelSV <svjonel@gmail.com>
2021-09-13 22:28:43 -0400

    'commit'

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    
+        return (
+            <div>
+                <h2>About</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, odit?</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, odit?</p>
+            </div>
+        );
+    }
+
+
+export default About;

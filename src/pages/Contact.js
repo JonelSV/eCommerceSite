6a49ad3e82a72110122c9f3d7c32121c1f0ec7dd
JonelSV <svjonel@gmail.com>
2021-09-13 22:29:09 -0400

    'commit'

---
+++ src/pages/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Contact</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum, animi.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum, animi.</p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
